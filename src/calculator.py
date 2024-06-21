bbe92bab81f004e2dab1c3140e176d0d7111fd41
sannith514 <anug.sannith@gmail.com>
2024-06-21 14:07:30 -0700

    Resolve merge conflict in calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -5,10 +5,7 @@ def subtract(a, b):
     return a - b
 
 def divide(a, b):
-<<<<<<< Updated upstream
     return a / b
-=======
     if b == 0:
         print("Cannot be divided by zero")
     return a / b
->>>>>>> Stashed changes
