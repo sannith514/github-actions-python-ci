44734e836d310e2cba26f5ab1f982c5c80d92251
sannith514 <anug.sannith@gmail.com>
2024-06-21 14:04:03 -0700

    Resolve conflicts and apply local changes

--- src/calculator.py
+++ src/calculator.py
@@ -5,4 +5,10 @@ def subtract(a, b):
     return a - b
 
 def divide(a, b):
+<<<<<<< Updated upstream
     return a / b
+=======
+    if b == 0:
+        print("Cannot be divided by zero")
+    return a / b
+>>>>>>> Stashed changes
